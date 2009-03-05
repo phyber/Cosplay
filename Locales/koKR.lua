b7c3ff4f9edf4aac3ddd1e3c035ff64dafa2f45e
phyber <phyber@wowace>
2009-03-05 12:24:31 +0000

    Add koKR translation submitted by jiyoon

---
+++ Locales/koKR.lua
@@ -0,0 +1,7 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Cosplay", "koKR")
+if not L then return end
+--
+L["Undress"] = "벗기"
+L["Target"] = "대상"
+L["Open the DressUpFrame"] = "미리보기 창 열기"
+L["Undress Button"] = "벗기 버튼"
