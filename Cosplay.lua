e2d46185ef7de299c4b9a3eb536557f848f0dfa9
phyber <phyber@wowace>
2019-09-15 18:23:36 +0100

    Convert tabs to spaces

--- Cosplay.lua
+++ Cosplay.lua
@@ -40,112 +40,112 @@ do
 end
 
 function Cosplay:CreateMainButtons()
-	if not MainButtonsCreated then
-		-- Undress button.  Lets get nekkid!
-		local DUFUndressButton = CreateFrame("Button", "DUFUndressButton", DressUpFrame, "UIPanelButtonTemplate")
-		DUFUndressButton:SetWidth(80)
-		DUFUndressButton:SetHeight(22)
-		DUFUndressButton:SetText(L["Undress"])
-		DUFUndressButton:SetPoint("RIGHT", "DressUpFrameResetButton", "LEFT", 0, 0)
-		DUFUndressButton:SetFrameStrata("HIGH")
-		DUFUndressButton:SetScript("OnClick", function()
+    if not MainButtonsCreated then
+        -- Undress button.  Lets get nekkid!
+        local DUFUndressButton = CreateFrame("Button", "DUFUndressButton", DressUpFrame, "UIPanelButtonTemplate")
+        DUFUndressButton:SetWidth(80)
+        DUFUndressButton:SetHeight(22)
+        DUFUndressButton:SetText(L["Undress"])
+        DUFUndressButton:SetPoint("RIGHT", "DressUpFrameResetButton", "LEFT", 0, 0)
+        DUFUndressButton:SetFrameStrata("HIGH")
+        DUFUndressButton:SetScript("OnClick", function()
             DressUpModel:Undress()
-			PlaySound(GS_TITLE_OPTION_OK)
-		end)
+            PlaySound(GS_TITLE_OPTION_OK)
+        end)
 
-		-- Target button.
-		local DUFDressTargetButton = CreateFrame("Button", "DUFDressTargetButton", DressUpFrame, "UIPanelButtonTemplate")
-		DUFDressTargetButton:SetWidth(80)
-		DUFDressTargetButton:SetHeight(22)
-		DUFDressTargetButton:SetText(L["Target"])
-		DUFDressTargetButton:SetPoint("RIGHT", "DUFUndressButton", "LEFT", 0, 0)
-		DUFDressTargetButton:SetFrameStrata("HIGH")
-		DUFDressTargetButton:SetScript("OnClick", function()
-			self:DressUpTarget()
-		end)
-		DUFDressTargetButton:SetScript("OnShow", function()
-			self:Reset()
-		end)
+        -- Target button.
+        local DUFDressTargetButton = CreateFrame("Button", "DUFDressTargetButton", DressUpFrame, "UIPanelButtonTemplate")
+        DUFDressTargetButton:SetWidth(80)
+        DUFDressTargetButton:SetHeight(22)
+        DUFDressTargetButton:SetText(L["Target"])
+        DUFDressTargetButton:SetPoint("RIGHT", "DUFUndressButton", "LEFT", 0, 0)
+        DUFDressTargetButton:SetFrameStrata("HIGH")
+        DUFDressTargetButton:SetScript("OnClick", function()
+            self:DressUpTarget()
+        end)
+        DUFDressTargetButton:SetScript("OnShow", function()
+            self:Reset()
+        end)
 
-		-- All done
-		MainButtonsCreated = true
+        -- All done
+        MainButtonsCreated = true
 
-		-- Unhook the script now.
-		self:Unhook(DressUpFrame, "OnShow")
-	end
+        -- Unhook the script now.
+        self:Unhook(DressUpFrame, "OnShow")
+    end
 end
 
 function Cosplay:CreateAHButtons()
-	if not AHButtonsCreated then
-		local AuctionDUFUndressButton = CreateFrame("Button", "ADUFUndressButton", SideDressUpModel, "UIPanelButtonTemplate")
-		AuctionDUFUndressButton:SetWidth(70)
-		AuctionDUFUndressButton:SetHeight(22)
-		AuctionDUFUndressButton:SetText(L["Undress"])
-		AuctionDUFUndressButton:SetPoint("BOTTOM", "SideDressUpModelResetButton", "TOP", 0, 2)
-		AuctionDUFUndressButton:SetScript("OnClick", function()
-			SideDressUpModel:Undress()
-			PlaySound(GS_TITLE_OPTION_OK)
-		end)
+    if not AHButtonsCreated then
+        local AuctionDUFUndressButton = CreateFrame("Button", "ADUFUndressButton", SideDressUpModel, "UIPanelButtonTemplate")
+        AuctionDUFUndressButton:SetWidth(70)
+        AuctionDUFUndressButton:SetHeight(22)
+        AuctionDUFUndressButton:SetText(L["Undress"])
+        AuctionDUFUndressButton:SetPoint("BOTTOM", "SideDressUpModelResetButton", "TOP", 0, 2)
+        AuctionDUFUndressButton:SetScript("OnClick", function()
+            SideDressUpModel:Undress()
+            PlaySound(GS_TITLE_OPTION_OK)
+        end)
 
-		AHButtonsCreated = true
-	end
+        AHButtonsCreated = true
+    end
 
-	self:UnregisterEvent("AUCTION_HOUSE_SHOW")
+    self:UnregisterEvent("AUCTION_HOUSE_SHOW")
 end
 
 function Cosplay:Reset()
-	local race, fileName = UnitRace("player")
+    local race, fileName = UnitRace("player")
 
-	SetPortraitTexture(DressUpFramePortrait, "player")
-	SetDressUpBackground(DressUpFrame, fileName)
-	DressUpModel:SetUnit("player")
+    SetPortraitTexture(DressUpFramePortrait, "player")
+    SetDressUpBackground(DressUpFrame, fileName)
+    DressUpModel:SetUnit("player")
 end
 
 local function DressUpTargetTexturePath()
-	local fileName = select(2, UnitRace("target"))
-	if fileName then
-		if string_lower(fileName) == "gnome" then
-			fileName = "Dwarf"
-		elseif string_lower(fileName) == "troll" then
-			fileName = "Orc"
-		end
-	else
-		fileName = "Orc"
-	end
+    local fileName = select(2, UnitRace("target"))
+    if fileName then
+        if string_lower(fileName) == "gnome" then
+            fileName = "Dwarf"
+        elseif string_lower(fileName) == "troll" then
+            fileName = "Orc"
+        end
+    else
+        fileName = "Orc"
+    end
 
-	return "Interface\\DressUpFrame\\DressUpBackground-"..fileName
+    return "Interface\\DressUpFrame\\DressUpBackground-"..fileName
 end
 
 local function SetDressUpTargetBackground()
-	local texture = DressUpTargetTexturePath()
-	DressUpBackgroundTopLeft:SetTexture(texture..1)
-	DressUpBackgroundTopRight:SetTexture(texture..2)
-	DressUpBackgroundBotLeft:SetTexture(texture..3)
-	DressUpBackgroundBotRight:SetTexture(texture..4)
+    local texture = DressUpTargetTexturePath()
+    DressUpBackgroundTopLeft:SetTexture(texture..1)
+    DressUpBackgroundTopRight:SetTexture(texture..2)
+    DressUpBackgroundBotLeft:SetTexture(texture..3)
+    DressUpBackgroundBotRight:SetTexture(texture..4)
 end
 
 function Cosplay:DressUpTarget()
-	if not DressUpFrame:IsVisible() then
-		ShowUIPanel(DressUpFrame)
-	else
-		PlaySound(GS_TITLE_OPTION_OK)
-	end
+    if not DressUpFrame:IsVisible() then
+        ShowUIPanel(DressUpFrame)
+    else
+        PlaySound(GS_TITLE_OPTION_OK)
+    end
 
-	if UnitIsVisible("target") then
-		SetPortraitTexture(DressUpFramePortrait, "target")
-		SetDressUpTargetBackground()
-		DressUpModel:SetUnit("target")
-	else
-		self:Reset()
-	end
+    if UnitIsVisible("target") then
+        SetPortraitTexture(DressUpFramePortrait, "target")
+        SetDressUpTargetBackground()
+        DressUpModel:SetUnit("target")
+    else
+        self:Reset()
+    end
 end
 
 function Cosplay:OnEnable()
-	if not AHButtonsCreated then
-		self:RegisterEvent("AUCTION_HOUSE_SHOW", "CreateAHButtons")
-	end
+    if not AHButtonsCreated then
+        self:RegisterEvent("AUCTION_HOUSE_SHOW", "CreateAHButtons")
+    end
 
-	if not MainButtonsCreated then
-		self:HookScript(DressUpFrame, "OnShow", "CreateMainButtons")
-	end
+    if not MainButtonsCreated then
+        self:HookScript(DressUpFrame, "OnShow", "CreateMainButtons")
+    end
 end
